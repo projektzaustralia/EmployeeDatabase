9c9d6520b35db32d33f4c0e3c7319f071cd45823
David Zammit <projektzaustralia@gmail.com>
2014-05-06 21:15:31 +1000

    AcademicEmployee in progress

--- EmployeeDatabase/src/AcademicEmployee.java
+++ EmployeeDatabase/src/AcademicEmployee.java
@@ -1,12 +1,14 @@
 
 public class AcademicEmployee extends Employee {
-   
-   private static Boolean hasPHD;
-	
+   	
    public AcademicEmployee(String employeeNumber, String name, String role,
-			char level) {
+			char level, Boolean hasPHD) {
 		super(employeeNumber, name, role, level);
+		
+		this.employeeNumber = employeeNumber;
+      this.name = name;
+      this.role = role;
+      this.level = level;
 
    }
-
 }
